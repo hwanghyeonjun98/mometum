d087367472155c0e1467b1993e49c5db5d2fc2b4
JJun <jjun1010@kakao.com>
2023-07-10 20:08:46 +0900

    firset commit

---
+++ src/components/Time.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const hour = ref();
+const minute = ref();
+const second = ref();
+const ampm = ref("");
+
+onMounted(() => {
+	setInterval(() => {
+		const date = new Date();
+		hour.value = date.getHours();
+		minute.value = date.getMinutes();
+		second.value = date.getSeconds();
+
+		ampm.value = "pm";
+	}, 1000);
+});
+
+</script>
+
+<template>
+	<div>
+		<ul>
+			<li>{{ hour }}</li>
+			<li>{{ minute }}</li>
+			<li>{{ second }}</li>
+			<li>{{ ampm }}</li>
+		</ul>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
