a99cb635a6331e69577874671dbce3b8df0d137f
HyeonJun <widrn1010@live.co.kr>
2023-07-14 13:33:01 +0900

    build

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,16 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins : [
+	plugins    : [
 		vue(),
 	],
-	resolve : {
+	resolve    : {
 		alias : {
 			"@" : fileURLToPath(new URL("./src", import.meta.url)),
 		},
 	},
-	build   : {
-		outDir     : "./docs",
-		publicPath : "/mometum/",
+	build      : {
+		outDir : "./docs",
 	},
+	publicPath : "/mometum",
 });
